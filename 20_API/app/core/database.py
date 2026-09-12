1be38d42a966c13bf486b14f9809f00557bcc4c4
RahulParmar311197 <mylearning311997@gmail.com>
2026-09-12 14:50:42 +0530

    lint: remove unused database Base import

--- 20_API/app/core/database.py
+++ 20_API/app/core/database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from app.core.config import get_settings
-from app.core.database_base import Base
 
 
 settings = get_settings()
